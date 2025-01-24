7d51512f9197c9faf4b8280bcfb150afa6ac1011
Christian Bartram <cbartram3@gmail.com>
2025-01-24 10:35:49 -0500

    feat(backups): Add automated backups container

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# hearthhub-sidecar
-A sidecar container which persists world files, backups, and metrics to S3 for a Valheim dedicated server.
+# HearthHub Sidecar
+A sidecar container which persists world files, backups, and metrics to S3 for a Valheim dedicated server. This application
+is designed to be run in a container which is on the **same** pod as the Valheim Dedicated Server.
+
+## Building
+
+To build the docker image  for the Valheim server run: `./build.sh 0.0.1` replacing `0.0.1` with
+the image version you would like to use.
+
+## Deployment
+
+This container is not designed to be deployed in an isolated manner as it depends on volumes and AWS credentials being
+present on the Valheim dedicated server pod. Therefore, it should be integrated and updated with the [HearthHub mod API](https://github.com/cbartram/hearthhub-mod-api) 
+which is responsible for actually creating the server deployment on Kubernetes.
+
+## Built With
+
+- [Kubernetes](https://kubernetes.io) - Container orchestration platform
+- [Helm](https://helm.sh) - Manages Kubernetes deployments
+- [Docker](https://docker.io/) - Container build tool
+- [Steam](https://steam.com) - CLI used to install Valheim dedicated server
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code
+of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [Semantic Versioning](http://semver.org/) for versioning. For the versions
+available, see the [tags on this
+repository](https://github.com/cbartran/hearthhub-mod-api/tags).
+
+## Authors
+
+- **cbartram** - *Initial work* -
+  [cbartram](https://github.com/cbartram)
+
+## License
+
+This project is licensed under the [CC0 1.0 Universal](LICENSE)
+Creative Commons License - see the [LICENSE.md](LICENSE) file for
+details
---
+++ main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	log "github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	sourceDir    = "/root/.config/unity3d/IronGate/Valheim"
+	s3BucketName = "hearthhub-backups"
+	backupPrefix = "valheim-backups-auto/"
+	backupFreq   = 10 * time.Minute
+)
+
+func main() {
+	logger := log.New()
+	logger.SetFormatter(&log.TextFormatter{
+		FullTimestamp: false,
+	})
+
+	logLevel, err := log.ParseLevel(os.Getenv("LOG_LEVEL"))
+	if err != nil {
+		logLevel = log.InfoLevel
+	}
+
+	log.SetOutput(os.Stdout)
+	log.SetLevel(logLevel)
+
+	log.Info("Starting Valheim server backup sidecar")
+	ctx := context.Background()
+
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Fatalf("unable to load AWS SDK config: %v", err)
+	}
+
+	s3Client := s3.NewFromConfig(cfg)
+	ticker := time.NewTicker(backupFreq)
+	defer ticker.Stop()
+
+	discordID, err := GetPodLabel("tenant-discord-id")
+	if err != nil {
+		log.Fatalf("Failed to get Discord ID: %v", err)
+	}
+
+	log.Infof("Tenant Discord ID: %s", discordID)
+
+	// Perform an initial backup on server start.
+	err = BackupWorldSaves(ctx, s3Client, discordID)
+	if err != nil {
+		log.Errorf("initial backup failed: %v", err)
+	}
+
+	for {
+		select {
+		case <-ticker.C:
+			if err = BackupWorldSaves(ctx, s3Client, discordID); err != nil {
+				log.Printf("backup failed: %v", err)
+			}
+		}
+	}
+}
+
+// GetPodLabel retrieves a label from a pod given the label key. Returns "" if no label can be found or
+// an error occurs.
+func GetPodLabel(labelKey string) (string, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return "", fmt.Errorf("error creating in-cluster config: %v", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return "", fmt.Errorf("error creating Kubernetes client: %v", err)
+	}
+
+	podName := os.Getenv("HOSTNAME")
+	pod, err := clientset.CoreV1().Pods("hearthhub").Get(context.Background(), podName, metav1.GetOptions{})
+	if err != nil {
+		return "", fmt.Errorf("error retrieving pod: %v", err)
+	}
+
+	labelValue, exists := pod.Labels[labelKey]
+	if !exists {
+		return "", fmt.Errorf("label %s not found on pod", labelKey)
+	}
+
+	return labelValue, nil
+}
+
+// BackupWorldSaves Persists located *.fwl and *.db files to S3 periodically.
+func BackupWorldSaves(ctx context.Context, s3Client *s3.Client, discordId string) error {
+	files, err := FindWorldFiles(sourceDir)
+	if err != nil {
+		return fmt.Errorf("error finding world files: %v", err)
+	}
+
+	for _, file := range files {
+		s3Key := fmt.Sprintf("%s/%s/%s", backupPrefix, discordId, filepath.Base(file))
+		fileData, err := os.Open(file)
+		if err != nil {
+			log.Infof("Failed to open file %s: %v", file, err)
+			continue
+		}
+		defer fileData.Close()
+
+		// Upload to S3
+		_, err = s3Client.PutObject(ctx, &s3.PutObjectInput{
+			Bucket: aws.String(s3BucketName),
+			Key:    aws.String(s3Key),
+			Body:   fileData,
+		})
+
+		if err != nil {
+			log.Printf("failed to upload backup %s to S3: %v", file, err)
+			continue
+		}
+
+		log.Printf("successfully backed up %s to s3://%s/%s", file, s3BucketName, s3Key)
+	}
+
+	return nil
+}
+
+// FindWorldFiles Recursively locates the *.fwl and *.db files on the PVC.
+func FindWorldFiles(dir string) ([]string, error) {
+	var worldFiles []string
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			ext := filepath.Ext(path)
+			if ext == ".fwl" || ext == ".db" {
+				worldFiles = append(worldFiles, path)
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	log.Infof("found: %v world files", len(worldFiles))
+	return worldFiles, nil
+}
